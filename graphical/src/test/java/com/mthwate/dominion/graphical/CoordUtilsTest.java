131a768521b7bcbb3e380630afde4d4acb366f41
Matthew Benkart <mthwate@mthwate.com>
2015-02-19 11:51:42 -0500

    Updated build scripts

---
+++ graphical/src/test/java/com/mthwate/dominion/graphical/CoordUtilsTest.java
@@ -0,0 +1,18 @@
+package com.mthwate.dominion.graphical;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CoordUtilsTest {
+
+	@Test
+	public void testCartesianToHex() throws Exception {
+
+	}
+
+	@Test
+	public void testHexToCartesian() throws Exception {
+
+	}
+}
