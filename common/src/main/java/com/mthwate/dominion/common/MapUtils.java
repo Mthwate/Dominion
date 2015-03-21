d815f967c74ed24ac0dc79ad5bbf12bf4166adb1
Matthew Benkart <mthwate@mthwate.com>
2015-03-21 12:17:13 -0400

    Added MapUtils which performs more complex functions on the TileStore

---
+++ common/src/main/java/com/mthwate/dominion/common/MapUtils.java
@@ -0,0 +1,30 @@
+package com.mthwate.dominion.common;
+
+import com.jme3.system.NanoTimer;
+import com.jme3.system.Timer;
+import com.mthwate.dominion.common.entity.Entity;
+import lombok.extern.java.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author mthwate
+ */
+@Log
+public class MapUtils {
+
+	public static List<Entity> getAllInhabitants() {
+		Timer t = new NanoTimer();
+		List<Entity> inhabitants = new ArrayList<>();
+		for (int x = 0; x < TileStore.sizeX(); x++) {
+			for (int y = 0; y < TileStore.sizeY(); y++) {
+				if (TileStore.get(x, y).hasInhabitant()) {
+					inhabitants.add(TileStore.get(x, y).getInhabitant());
+				}
+			}
+		}
+		return inhabitants;
+	}
+
+}
