85ad13226f2de682839315b3abe3403d908c36a5
Matthew Benkart <mthwate@mthwate.com>
2015-03-21 18:24:52 -0400

    Started a big restructure of the project

---
+++ desktop/client/src/main/java/com/mthwate/dominion/client/ClientMain.java
@@ -0,0 +1,14 @@
+package com.mthwate.dominion.client;
+
+import com.mthwate.dominion.common.Starter;
+
+/**
+ * @author mthwate
+ */
+public class ClientMain {
+
+	public static void main(String[] args) {
+		Starter.start(new ClientApp(), true, "Dominion");
+	}
+
+}
