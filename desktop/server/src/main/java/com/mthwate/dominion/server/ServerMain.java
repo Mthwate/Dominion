e1b4e447ac5528a700e55f3fcb2e5981574526f1
Matthew Benkart <mthwate@mthwate.com>
2015-03-22 15:29:39 -0400

    Small project restructure

--- desktop/server/src/main/java/com/mthwate/dominion/server/ServerMain.java
+++ desktop/server/src/main/java/com/mthwate/dominion/server/ServerMain.java
@@ -6,9 +6,9 @@ import com.mthwate.dominion.common.Starter;
  * @author mthwate
  */
 public class ServerMain {
-	
+
 	public static void main(String[] args) {
 		Starter.start(new ServerApp(), false, null);
 	}
-	
+
 }
