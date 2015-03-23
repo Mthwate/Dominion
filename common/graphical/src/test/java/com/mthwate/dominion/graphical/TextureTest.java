dd703ef690bc021eff9456350584babb31e2a298
Matthew Benkart <mthwate@mthwate.com>
2015-03-22 20:39:32 -0400

    Added a test to ensure textures have dimensions that are powers of 2

---
+++ common/graphical/src/test/java/com/mthwate/dominion/graphical/TextureTest.java
@@ -0,0 +1,26 @@
+package com.mthwate.dominion.graphical;
+
+import com.mthwate.datlib.IOUtils;
+import com.mthwate.dominion.common.CoordUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class TextureTest {
+
+	@Test
+	public void isPowTwo() throws IOException {
+		File dir = new File("src/main/resources/textures");
+		for (File file : dir.listFiles()) {
+			BufferedImage img = ImageIO.read(file);
+			int width = img.getWidth();
+			int height = img.getHeight();
+			Assert.assertTrue((width & -width) == width);//is pow of 2
+			Assert.assertTrue((height & -height) == height);//is pow of 2
+		}
+	}
+}
