04943d96102f0a00504ff231135cd8339f5d5195
Matthew Benkart <mthwate@mthwate.com>
2015-04-04 20:33:43 -0400

    Fixed compile error

--- common/common/src/main/java/com/mthwate/dominion/common/tile/Tile.java
+++ common/common/src/main/java/com/mthwate/dominion/common/tile/Tile.java
@@ -12,7 +12,7 @@ public class Tile implements java.io.Serializable {
 
 	private static final long serialVersionUID = 6211327075280365043L;
 
-	private String type;
+	@Setter private String type;
 
 	@Getter @Setter private int elevation;
 	
